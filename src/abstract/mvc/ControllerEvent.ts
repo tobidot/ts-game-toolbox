ae63acb93686feb94319ef29adf534a40d8883eb
tobidot <Object.name@live.de>
2021-02-06 20:12:36 +0100

    Special interface guard for event requests

--- src/abstract/mvc/ControllerEvent.ts
+++ src/abstract/mvc/ControllerEvent.ts
@@ -4,5 +4,9 @@ export interface ControllerEvent {
 }
 
 export function is_controller_event(object: any): object is ControllerEvent {
-    return typeof object === "object" && "event_name" in object;
+    return is_controller_event(object);
+}
+
+export function is_specific_controller_event<T extends ControllerEvent>(object: any, name: T["event_name"]): object is T {
+    return is_controller_event(object) && (!name || object.event_name === name);
 }
