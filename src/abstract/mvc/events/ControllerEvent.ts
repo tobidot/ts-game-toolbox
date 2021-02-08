06ad568d6729c79696e8ec4a18990c496ff90aa1
tobidot <Object.name@live.de>
2021-02-08 22:13:34 +0100

    fix is_controller_event funciton

--- src/abstract/mvc/events/ControllerEvent.ts
+++ src/abstract/mvc/events/ControllerEvent.ts
@@ -4,7 +4,7 @@ export interface ControllerEvent {
 }
 
 export function is_controller_event(object: any): object is ControllerEvent {
-    return is_controller_event(object);
+    return typeof object === "object" && object.hasOwnProperty('event_name');
 }
 
 export function is_specific_controller_event<T extends ControllerEvent>(object: any, name: T["event_name"]): object is T {
